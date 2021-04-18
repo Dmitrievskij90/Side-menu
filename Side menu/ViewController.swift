447fb636a8886b88ec24e0861557484c7b740593
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-04-18 18:42:10 +0600

    Initial commit

---
+++ Side menu/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Side menu
+//
+//  Created by Konstantin Dmitrievskiy on 18.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
